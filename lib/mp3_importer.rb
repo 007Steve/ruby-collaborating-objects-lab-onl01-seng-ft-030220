0b9581bd57be819a593908f43f1786f271a12661
Stephen Plummer <stephen.plummer1.sp@gmail.com>
2020-03-23 14:28:16 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -7,11 +7,19 @@ class MP3Importer
   end
 
   def files
-    @files ||= Dir.entries(@path).select {|song| !File.directory?(song) && song.end_with?(".mp3")}
+    @files ||= Dir.glob("#{path}/*.mp3").collect do |file|
+      file.gsub("#{path}/", "")
+    end
   end
 
+ 
+
   def import
-    files.each {|song| Song.new_by_filename(song)}
+    files.each{ | file | Song.new_by_filename(file)}
   end
+
+ 
+
+
 
 end
